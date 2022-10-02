3414b59733e3ff1ae3bc12ed30930bd9b50364b3
mmoger <marina.moger@gmail.com>
2022-10-02 13:36:03 +0300

    getBooks Test is done

--- src/test/java/lv/marmog/zone2/zone2/services/implementation/BookServiceTest.java
+++ src/test/java/lv/marmog/zone2/zone2/services/implementation/BookServiceTest.java
@@ -3,7 +3,9 @@ package lv.marmog.zone2.zone2.services.implementation;
 import lv.marmog.zone2.zone2.DTO.BookDTO;
 import lv.marmog.zone2.zone2.mappers.BookMapper;
 import lv.marmog.zone2.zone2.models.Book;
+import lv.marmog.zone2.zone2.models.errors.BookNotFoundException;
 import lv.marmog.zone2.zone2.repositories.BookRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,12 +15,16 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -38,64 +44,30 @@ class BookServiceTest {
     @Spy
     BookMapper mapper;
 
-    private Book book;
-    private BookDTO bookDTO;
-    private List<Book> bookList;
-    private List<BookDTO> bookDTOList;
+
+
+    private List <Book> bookList (){
+        List<Book> testList  = new ArrayList<>();
+        Date date = new Date();
+        testList.add(new Book(1, 100, "Name1", date, "Author1", true, "home"));
+        testList.add(new Book(2, 200, "Name2", date, "Author2", false, "home"));
+        return testList;
+    }
+    private List <BookDTO> bookDTOList (){
+        List<BookDTO> testList  = new ArrayList<>();
+        testList.add(new BookDTO(100, "Name1", "Author1", "home", true));
+        testList.add(new BookDTO(200, "Name2", "Author2", "home", false));
+        return testList;
+    }
+    Date date = new Date();
+    private Book book = new Book(1, 100, "Name1", date, "Author1", true, "home");
+    private BookDTO bookDTO = new BookDTO(100, "Name1", "Author1", "home", true);
+
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
-
-    @BeforeEach
-    void init() {
-        bookDTO = createBookDTO();
-        book = createBook();
-        bookDTOList = createBookDTOList();
-        bookList = createBookList();
-    }
-
-    private List<BookDTO> createBookDTOList() {
-        List<BookDTO> listDTO = new ArrayList<>();
-        BookDTO bookDTO1 = createBookDTO();
-        BookDTO bookDTO2 = createBookDTO();
-        listDTO.add(bookDTO1);
-        listDTO.add(bookDTO2);
-        return listDTO;
-    }
-    private BookDTO createBookDTO() {
-        BookDTO bookDTO = new BookDTO();
-        bookDTO.setBookCode(10);
-        bookDTO.setBookName("Name1");
-        bookDTO.setAuthor("Author1");
-        bookDTO.setLocation("location1");
-        bookDTO.setIsRead(true);
-        return bookDTO;
-    }
-
-    private List<Book> createBookList() {
-        List<Book> list = new ArrayList<>();
-        Book book1 = createBook();
-        Book book2 = createBook();
-        list.add(book1);
-        list.add(book2);
-        return list;
-    }
-
-    private Book createBook() {
-        Date date = new Date();
-        Book book = new Book();
-        book.setAuthor("Author1");
-        book.setBookCode(10);
-        book.setBookName("Name1");
-        book.setAddedOn(date);
-        book.setIsRead(true);
-        book.setLocation("location1");
-        return book;
-    }
-
-
 
 
     @Test
@@ -104,7 +76,8 @@ class BookServiceTest {
 
     @Test
     void getBooks() {
-        when(bookRepository.findAll()).thenReturn(bookList);
+        List<Book> testList = bookList();
+        when(bookRepository.findAll()).thenReturn(testList);
         when(mapper.BookToDTO(book)).thenReturn(bookDTO);
         List<BookDTO> books = bookService.getBooks();
         assertEquals(2, books.size());
@@ -113,6 +86,15 @@ class BookServiceTest {
 
     @Test
     void getBookByCode() {
+
+        when(bookRepository.getBookByCode(anyInt())).thenReturn(Optional.of(book));
+        when(mapper.BookToDTO(book)).thenReturn(bookDTO);
+        BookDTO bookByCode = bookService.getBookByCode(book.getBookCode());
+        assertEquals(bookDTO.getBookCode(), bookByCode.getBookCode());
+        assertEquals(bookDTO.getBookName(), bookByCode.getBookName());
+        assertEquals(bookDTO.getAuthor(), bookByCode.getAuthor());
+        assertEquals(bookDTO.getBookName(), bookByCode.getBookName());
+        verify(bookRepository, times(1)).findAll();
     }
 
     @Test
