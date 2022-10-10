0c4ce3bc34e03f26b1b0c9ca1329e2d36dd7543f
mmoger <marina.moger@gmail.com>
2022-10-10 17:20:39 +0300

    added atatic data for service and controller tests and started controller tests

---
+++ src/test/java/lv/marmog/zone2/zone2/DataForTests.java
@@ -0,0 +1,36 @@
+package lv.marmog.zone2.zone2;
+
+import lv.marmog.zone2.zone2.DTO.BookDTO;
+import lv.marmog.zone2.zone2.models.Book;
+
+import java.util.ArrayList;
+import java.util.Date;
+public class DataForTests {
+
+    public static ArrayList<Book> bookList() {
+        ArrayList<Book> testList = new ArrayList<>();
+        Date date = new Date();
+        testList.add(new Book(1, 100, "Name1", date, "Author1", true, "home"));
+        testList.add(new Book(2, 200, "Name2", date, "Author2", false, "home"));
+        return testList;
+    }
+
+    public static ArrayList<BookDTO> bookDTOList() {
+        ArrayList<BookDTO> testList = new ArrayList<>();
+        testList.add(new BookDTO(100, "Name1", "Author1", "home", true));
+        testList.add(new BookDTO(200, "Name2", "Author2", "home", false));
+        return testList;
+    }
+
+    static Date date = new Date();
+    public static Book book(){
+        Book book = new Book(1, 100, "Name1", date, "Author1", true, "home");
+        return book;
+    }
+
+
+    public static BookDTO bookDTO(){
+        BookDTO bookDTO = new BookDTO(100, "Name1", "Author1", "home", true);
+        return bookDTO;
+    }
+}
---
+++ src/test/java/lv/marmog/zone2/zone2/controllers/BookControllerTest.java
@@ -0,0 +1,80 @@
+package lv.marmog.zone2.zone2.controllers;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lv.marmog.zone2.zone2.DTO.BookDTO;
+import lv.marmog.zone2.zone2.services.interfaces.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+
+import static lv.marmog.zone2.zone2.DataForTests.bookDTO;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@ContextConfiguration(classes = {BookController.class})
+@ExtendWith(SpringExtension.class)
+class BookControllerTest {
+
+    @Autowired
+    private BookController bookController;
+
+
+    @MockBean
+    BookService bookService;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getBook() {
+    }
+
+    @Test
+    void createNewBook() {
+    }
+
+    @Test
+    void updateBook() throws Exception {
+        when(bookService.updateBook((BookDTO) any(), anyInt())).thenReturn(new BookDTO());
+        BookDTO bookDTO = bookDTO();
+        String content = (new ObjectMapper()).writeValueAsString(bookDTO);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/book/{bookCode}", 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(bookController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                .string("{\"bookCode\":null,\"bookName\":null,\"author\":null,\"location\":null,\"isRead\":null}"));
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void getBooksByAuthor() {
+    }
+}
